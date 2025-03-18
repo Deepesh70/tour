5aa7e8aee27b009f75ffdbde34c9c332d8fbf6ab
Deepesh <deepeshdangi700@gmail.com>
2025-03-18 14:42:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+// src/App.js
 import YatraSignup from './YatraSignup';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import Explore from './Explore'; // Create these components
+import Flights from './Flights';
+import Hotels from './Hotels';
+import Trains from './Trains';
+import Taxis from './Taxis';
+import Rentals from './Rentals';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <YatraSignup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/flights" element={<Flights />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/trains" element={<Trains />} />
+        <Route path="/taxis" element={<Taxis />} />
+        <Route path="/rentals" element={<Rentals />} />
+        <Route path="/signup" element={<YatraSignup />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Trains.js
@@ -0,0 +1,13 @@
+// src/components/Trains.js
+import React from 'react';
+
+const Trains = () => {
+  return (
+    <div className="page">
+      <h1>Train Booking</h1>
+      <p>Book train tickets for your journey.</p>
+    </div>
+  );
+};
+
+export default Trains;
